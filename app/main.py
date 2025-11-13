eb341914f632cee6b8f39bfc02543d0b1206f519
giovall <giovanni.aiello1993@gmail.com>
2025-11-13 12:24:41 +0100

    add new cors setting

--- app/main.py
+++ app/main.py
@@ -156,6 +156,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "http://localhost:5173",
         "https://takash-psi.vercel.app"
     ],
     allow_credentials=True,
